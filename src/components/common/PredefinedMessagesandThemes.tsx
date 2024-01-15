351ac9ee509733251d7b1fb6f6ff2bbdef1ea0c4
Kyle Nhu <thanhtruong.kyle@gmail.com>
2024-01-14 22:17:01 -0500

    Change email content

--- src/components/common/PredefinedMessagesandThemes.tsx
+++ src/components/common/PredefinedMessagesandThemes.tsx
@@ -3,19 +3,21 @@ export const initialApplicationSubject =
 
 export const initialApplicationMessage = `Hi <First Name>,
 
-Thank you for your interest in our suite at Tranquility Place. My name is Kelly, property manager of Tranquility Place. I'm pleased to address your queries and assist you in the application process.
+Thank you for your interest in leasing with us at Tranquility Place PEI. My name is Kelly, a member of the property management team at Tranquility Place. I'm pleased to address your queries and assist you in the application process.
 
 We are delighted to inform you that we have scheduled a viewing for the suite you're interested in at Tranquility Place. Here are the details for your scheduled visit:
 
 - Date: [Insert Date]
 - Time: [Insert Time]
-- Location: Tranquility Place, 2 Birchwood Lane, Stratford, PEI C1B 3Z1.
+- Location: Tranquility Place PEI - 16 Elena Court, Charlottetown, PE C1C 0B2
+
+Please confirm that you are available for the listed time above by replying to this email.
 
 Please meet us at the front entrance where I, or a member of our team, will be there to greet you and show you around. The viewing will give you a great opportunity to explore the suite, experience the amenities of our building, and get a feel for the neighborhood.
 
 We recommend you prepare any questions you might have about the suite or the building, as we will be more than happy to answer them during your visit.
 
-If you need to reschedule or have any questions prior to the viewing, please don’t hesitate to contact us at (902) 393 - 2826 or tranquilityplacepei@gmail.com.
+If you need to reschedule or have any questions prior to the viewing, please don’t hesitate to contact us at (902) 314 - 6827 or tranquilityplacepei@gmail.com.
 
 Looking forward to meeting you and showing you what our community has to offer!\n
 
@@ -24,13 +26,13 @@ Best regards,
 
 Tranquility Place PEI
 Peace Property Management Ltd.
-Cell: (902) 393 - 2826
+Cell: (902) 314 - 6827
 Email: tranquilityplacepei@gmail.com
 Website: https://tranquilityplacepei.ca`;
 
 export const initialInquirySubject = "Response to Your Inquiry About Our Suite";
 
-export const initialInquiryMessage = `Hi <First Name>,\n\nThank you for your interest in our suite at Tranquility Place. My name is Kelly, property manager of Tranquility Place. I'm pleased to address your queries and assist you in the application process.\n\n<Replace with your message...>\n\nShould you have any more questions or wish to schedule a viewing, please feel free to contact me. We look forward to possibly having you as part of our community!\n\n\nBest regards,\n\nTranquility Place PEI\nPeace Property Management  Ltd.\nCell: (902) 393 - 2826\nEmail: tranquilityplacepei@gmail.com\nWebsite: https://tranquilityplacepei.ca`;
+export const initialInquiryMessage = `Hi <First Name>,\n\nThank you for your interest in our suites at Tranquility Place PEI. My name is Kelly, a member of the property management team at Tranquility Place. I'm pleased to address your queries and assist you in the application process.\n\n<Replace with your message...>\n\nShould you have any more questions or wish to schedule a viewing, please feel free to contact me. We look forward to possibly having you as part of our community!\n\n\nBest regards,\n\nTranquility Place PEI\nPeace Property Management  Ltd.\nCell: (902) 314 - 6827\nEmail: tranquilityplacepei@gmail.com\nWebsite: https://tranquilityplacepei.ca`;
 
 export const initialWaitlistSubject =
   "Welcome to the Tranquility Place Waitlist!";
@@ -51,7 +53,7 @@ Warm regards,
 
 Tranquility Place PEI
 Peace Property Management Ltd.
-Cell: (902) 393 - 2826
+Cell: (902) 314 - 6827
 Email: tranquilityplacepei@gmail.com
 Website: https://tranquilityplacepei.ca`;
 
